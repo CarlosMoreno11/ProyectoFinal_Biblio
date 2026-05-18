ad90199d0cbcf8c024142a508aaba0cf5e784d17
CarlosMoreno11 <carlosalcalaguillen@gmail.com>
2026-05-18 12:07:38 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,28 +6,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoDatabase;
 
 public class Main extends Application {
 
     static {
         System.setProperty("prism.order", "sw");
         System.setProperty("glass.accessible.force", "false");
-    }
-
-    @Override
-    public void init() {
-        System.out.println("Intentando conectar a MongoDB...");
-        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
-            MongoDatabase database = mongoClient.getDatabase("bibliotecapp");
-            long numLibros = database.getCollection("libros").countDocuments();
-            System.out.println("Conexión exitosa a MongoDB.");
-            System.out.println("Libros actuales: " + numLibros);
-        } catch (Exception e) {
-            System.err.println("Error MongoDB: " + e.getMessage());
-        }
     }
 
     @Override
@@ -38,7 +22,8 @@ public class Main extends Application {
                 Parent root = loader.load();
 
                 Scene scene = new Scene(root);
-                primaryStage.setTitle("Bibliotecapp - Panel de Control");                primaryStage.setScene(scene);
+                primaryStage.setTitle("Bibliotecapp - Panel de Control");
+                primaryStage.setScene(scene);
                 
                 primaryStage.show();
                 System.out.println("¡Interfaz cargada correctamente!");
